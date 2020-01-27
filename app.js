ac06fbb517cdccda0264e9566806c4466c46e908
unknown <hemanthn7.n7@gmail.com>
2020-01-27 11:01:27 +0000

    ApplicationInsights

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var appInsights = require('applicationinsights');
+appInsights.setup('0e59ded3-1914-4bce-b037-29212a69ebc8');
+appInsights.start();
+
 var app = express();
 
 // view engine setup
@@ -21,7 +25,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/problem',function()
+{
+throw new Error("oops!! Someting went wrong")
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -32,7 +39,8 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  
+  appInsights.defaultClient..trackException({exception : err});
   // render the error page
   res.status(err.status || 500);
   res.render('error');
